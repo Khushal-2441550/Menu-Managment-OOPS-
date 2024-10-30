1b8689b03fe9edae8b2ab6512a7f69b60fb1a448
Khushal-2441550 <s.khushal@bcah.christuniversity.in>
2024-10-30 10:10:47 +0530

    Update Menu Management.cpp

--- Menu Management.cpp
+++ Menu Management.cpp
@@ -5,35 +5,35 @@ using namespace std;
 // Base class: Cafe
 class Cafe {
 private:
-    static int nextOrderID; // Static variable for unique order IDs
-    int *espressoQty, *latteQty, *coffeeQty, *cappuccinoQty, *chocolateQty, *mochaQty; // Dynamic memory for quantities
+    static int nextorderid; // Static variable for unique order IDs
+    int *espressoqty, *latteqty, *coffeeqty, *cappuccinqty, *chocolateqty, *mochaqty; // Dynamic memory for quantities
 
 public:
-    int orderID, cost;
+    int orderid, cost;
 
     // Default Constructor
-    Cafe() : orderID(++nextOrderID), cost(0) {
-        espressoQty = new int(0);
-        latteQty = new int(0);
-        coffeeQty = new int(0);
-        cappuccinoQty = new int(0);
-        chocolateQty = new int(0);
-        mochaQty = new int(0);
+    Cafe() : orderid(++nextorderid), cost(0) {
+        espressoqty = new int(0);
+        latteqty = new int(0);
+        coffeeqty = new int(0);
+        cappuccinqty = new int(0);
+        chocolateqty = new int(0);
+        mochaqty = new int(0);
     }
 
     // Destructor for memory cleanup
     virtual ~Cafe() {
-        delete espressoQty;
-        delete latteQty;
-        delete coffeeQty;
-        delete cappuccinoQty;
-        delete chocolateQty;
-        delete mochaQty;
+        delete espressoqty;
+        delete latteqty;
+        delete coffeeqty;
+        delete cappuccinqty;
+        delete chocolateqty;
+        delete mochaqty;
     }
 
     // Static function to display total orders
-    static void showTotalOrders() {
-        cout << "Total Orders Placed: " << nextOrderID << endl;
+    static void showtotalorders() {
+        cout << "Total Orders Placed: " << nextorderid << endl;
     }
 
     // Menu display function
@@ -48,90 +48,90 @@ public:
     }
 
     // Adding items to the cart (pass by reference)
-    void addToCart(int id, int &qty) {
+    void addtocart(int id, int &qty) {
         switch (id) {
-            case 1: *espressoQty += qty; cost += qty * 200; break;
-            case 2: *latteQty += qty; cost += qty * 250; break;
-            case 3: *coffeeQty += qty; cost += qty * 300; break;
-            case 4: *cappuccinoQty += qty; cost += qty * 350; break;
-            case 5: *chocolateQty += qty; cost += qty * 400; break;
-            case 6: *mochaQty += qty; cost += qty * 450; break;
+            case 1: *espressoqty += qty; cost += qty * 200; break;
+            case 2: *latteqty += qty; cost += qty * 250; break;
+            case 3: *coffeeqty += qty; cost += qty * 300; break;
+            case 4: *cappuccinqty += qty; cost += qty * 350; break;
+            case 5: *chocolateqty += qty; cost += qty * 400; break;
+            case 6: *mochaqty += qty; cost += qty * 450; break;
             default: cout << "Invalid item ID\n"; break;
         }
     }
 
     // Virtual function to display order summary
-    virtual void displaySummary() const {
-        cout << "\nOrder Summary (Order ID: " << orderID << ")\n";
-        if (*espressoQty > 0) cout << "Espresso Qty: " << *espressoQty << ", Total: " << (*espressoQty * 200) << "\n";
-        if (*latteQty > 0) cout << "Latte Qty: " << *latteQty << ", Total: " << (*latteQty * 250) << "\n";
-        if (*coffeeQty > 0) cout << "Filter Coffee Qty: " << *coffeeQty << ", Total: " << (*coffeeQty * 300) << "\n";
-        if (*cappuccinoQty > 0) cout << "Cappuccino Qty: " << *cappuccinoQty << ", Total: " << (*cappuccinoQty * 350) << "\n";
-        if (*chocolateQty > 0) cout << "Hot Chocolate Qty: " << *chocolateQty << ", Total: " << (*chocolateQty * 400) << "\n";
-        if (*mochaQty > 0) cout << "Mocha Qty: " << *mochaQty << ", Total: " << (*mochaQty * 450) << "\n";
+    virtual void displaysummary() const {
+        cout << "\nOrder Summary (Order ID: " << orderid << ")\n";
+        if (*espressoqty > 0) cout << "Espresso Qty: " << *espressoqty << ", Total: " << (*espressoqty * 200) << "\n";
+        if (*latteqty > 0) cout << "Latte Qty: " << *latteqty << ", Total: " << (*latteqty * 250) << "\n";
+        if (*coffeeqty > 0) cout << "Filter Coffee Qty: " << *coffeeqty << ", Total: " << (*coffeeqty * 300) << "\n";
+        if (*cappuccinqty > 0) cout << "Cappuccino Qty: " << *cappuccinqty << ", Total: " << (*cappuccinqty * 350) << "\n";
+        if (*chocolateqty > 0) cout << "Hot Chocolate Qty: " << *chocolateqty << ", Total: " << (*chocolateqty * 400) << "\n";
+        if (*mochaqty > 0) cout << "Mocha Qty: " << *mochaqty << ", Total: " << (*mochaqty * 450) << "\n";
         cout << "Total Cost: " << cost << "\n";
     }
 
     // Apply discount to an order
-    void applyDiscount(int discountPercent) {
-        int discountAmount = (cost * discountPercent) / 100;
-        cost -= discountAmount;
-        cout << "Discount of " << discountPercent << "% applied. Final Cost: " << cost << "\n";
+    void applydiscount(int discountpercent) {
+        int discountamount = (cost * discountpercent) / 100;
+        cost -= discountamount;
+        cout << "Discount of " << discountpercent << "% applied. Final Cost: " << cost << "\n";
     }
 
     // Handle payment and calculate change
-    void processPayment() {
-        int moneyGiven;
+    void processpayment() {
+        int moneygiven;
         cout << "Enter money given by customer: ";
-        cin >> moneyGiven;
+        cin >> moneygiven;
 
-        while (moneyGiven < cost) {
-            cout << "Insufficient amount! Additional " << (cost - moneyGiven) << " required.\n";
-            int additionalAmount;
+        while (moneygiven < cost) {
+            cout << "Insufficient amount! Additional " << (cost - moneygiven) << " required.\n";
+            int additionalamount;
             cout << "Enter additional money: ";
-            cin >> additionalAmount;
-            moneyGiven += additionalAmount;
+            cin >> additionalamount;
+            moneygiven += additionalamount;
         }
 
-        int change = moneyGiven - cost;
+        int change = moneygiven - cost;
         cout << "Change to be returned: " << change << "\n";
         cout << "Thank you for ordering!\n";
     }
 };
 
 // Static member initialization
-int Cafe::nextOrderID = 0;
+int Cafe::nextorderid = 0;
 
 // Derived class
 class SpecialCafe : public Cafe {
 public:
-    void displaySummary() const override {
-        cout << "\n[Special Cafe] Order Summary (ID: " << orderID << ")\n";
-        Cafe::displaySummary();
+    void displaysummary() const override {
+        cout << "\n[Special Cafe] Order Summary (ID: " << orderid << ")\n";
+        Cafe::displaysummary();
         cout << "Special rates applied!\n";
     }
 };
 
 
 int main() {
-    int choice, qty, itemID, numOrders, discountPercent;
+    int choice, qty, itemid, numorders, discountpercent;
 
     cout << "Enter the number of orders: ";
-    cin >> numOrders;
+    cin >> numorders;
 
-    Cafe *orders = new SpecialCafe[numOrders]; // Dynamic allocation of orders
+    Cafe *orders = new SpecialCafe[numorders]; // Dynamic allocation of orders
 
     // Taking orders
-    for (int i = 0; i < numOrders; i++) {
+    for (int i = 0; i < numorders; i++) {
         cout << "\nOrder " << (i + 1) << ":\n";
         do {
             orders[i].menu();
             cout << "Enter item ID to order (1-6): ";
-            cin >> itemID;
-            if (itemID >= 1 && itemID <= 6) {
+            cin >> itemid;
+            if (itemid >= 1 && itemid <= 6) {
                 cout << "Enter quantity: ";
                 cin >> qty;
-                orders[i].addToCart(itemID, qty);
+                orders[i].addtocart(itemid, qty);
             } else {
                 cout << "Invalid item ID. Try again.\n";
             }
@@ -142,31 +142,30 @@ int main() {
 
     // Displaying all orders
     cout << "\n=== All Orders Summary ===\n";
-    for (int i = 0; i < numOrders; i++) {
+    for (int i = 0; i < numorders; i++) {
         cout << "\nOrder " << (i + 1) << " Summary:\n";
-        orders[i].displaySummary();
+        orders[i].displaysummary();
     }
 
     // Applying discount to all orders
     cout << "\nEnter discount percentage to apply to all orders: ";
-    cin >> discountPercent;
-    for (int i = 0; i < numOrders; i++) {
+    cin >> discountpercent;
+    for (int i = 0; i < numorders; i++) {
         cout << "\nApplying discount to Order " << (i + 1) << "...\n";
-        orders[i].applyDiscount(discountPercent);
+        orders[i].applydiscount(discountpercent);
     }
 
     // Processing payment for all orders
     cout << "\n=== Payment Processing ===\n";
-    for (int i = 0; i < numOrders; i++) {
+    for (int i = 0; i < numorders; i++) {
         cout << "\nProcessing payment for Order " << (i + 1) << "...\n";
-        orders[i].processPayment();
+        orders[i].processpayment();
     }
 
     // Display total orders placed
-    Cafe::showTotalOrders();
+    Cafe::showtotalorders();
 
     //deallocation of memory
     delete[] orders;
     return 0;
 }
-
